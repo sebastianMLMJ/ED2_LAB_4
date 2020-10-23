70d375c4325e3ee059a9a90523b52197ebb14cbd
Sebastian_Milian <milianmejiaedgarsebastian@yahoo.com>
2020-10-23 10:40:25 -0600

    Interfaz, algoritmo de compresion LZW y consola de pruebas

---
+++ ConsolaDePruebas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LZW;
+
+
+namespace ConsolaDePruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LZWCompresor compresor = new LZWCompresor();
+            compresor.Comprimir("C:\\Users\\Danilo_Toshiba\\Desktop\\ED2 LAB 4\\cuento.txt", "C:\\Users\\Danilo_Toshiba\\Desktop\\ED2 LAB 4\\compresion.txt", "C:\\Users\\Danilo_Toshiba\\Desktop\\ED2 LAB 4\\info.txt");
+            compresor.descomprimir("C:\\Users\\Danilo_Toshiba\\Desktop\\ED2 LAB 4\\compresion.txt", "C:\\Users\\Danilo_Toshiba\\Desktop\\ED2 LAB 4\\descompresion.txt");
+
+        }
+    }
+}
